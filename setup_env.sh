093ab88c4a9c2039379a9439cde6b065659a508e
5wjdgns2 <61228399+5wjdgns2@users.noreply.github.com>
2020-12-10 12:26:13 +0900

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -19,12 +19,11 @@ fi
 
 
 echo #install docker 1.13 version
-curl -fLsSv --retry 20 -Y 100000 -y 60 -o /tmp/docker-engine-1.13.1-1.el7.centos.x86_64.rpm \
-  https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-1.13.1-1.el7.centos.x86_64.rpm
-curl -fLsSv --retry 20 -Y 100000 -y 60 -o /tmp/docker-engine-selinux-1.13.1-1.el7.centos.noarch.rpm \
-  https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-selinux-1.13.1-1.el7.centos.noarch.rpm
-yum -y localinstall /tmp/docker*.rpm || true
-
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
 
 echo #########################
 echo #setup env
@@ -38,7 +37,7 @@ setenforce permissive
 
 echo #change setting sshd
 sed -i '/^\s*UseDNS /d' /etc/ssh/sshd_config
-echo -e "\nUseDNS no" >> /etc/ssh/sshd_config 
+echo -e "\nUseDNS no" >> /etc/ssh/sshd_config
 
 
 echo #set journald limits
@@ -90,4 +89,5 @@ localectl set-locale LANG=en_US.utf8
 echo #clean up
 yum clean all
 rm -rf /tmp/* /var/tmp/*
+
 
